4c912c30207e47c37d197348767d487dbe902b0e
evslav <evslav@bk.ru>
2017-07-29 22:16:13 +0300

    linux test

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(){
    // cout << FileInfo[0].size << endl;
 
     int q = 0;
-    QDirIterator it("C:\\Users\\evslav\\Pictures", QDirIterator::Subdirectories);
+    QDirIterator it("/home/evslav/Documents/Pictures/", QDirIterator::Subdirectories);
     while (it.hasNext()) {
         QFileInfo i = it.next();
         QString fileName = it.fileName();
